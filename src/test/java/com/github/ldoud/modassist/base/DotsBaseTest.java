58c08c326a83ec4db1403e57a5ffaa137d81f193
ldoud <leon.doud@gmail.com>
2018-03-25 16:51:45 -0400

    Cleaned up DotsBaseTest

--- src/test/java/com/github/ldoud/modassist/base/DotsBaseTest.java
+++ src/test/java/com/github/ldoud/modassist/base/DotsBaseTest.java
@@ -2,40 +2,32 @@ package com.github.ldoud.modassist.base;
 
 import com.github.ldoud.modassist.constants.Character;
 import com.github.ldoud.modassist.constants.Mod;
-import com.github.ldoud.modassist.swgoh.TestXmlData;
-import org.dom4j.Node;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import java.io.IOException;
 
 public abstract class DotsBaseTest {
 
     @Test
-    public void testOneDot() {
+    void testOneDot() {
         assertNumberOfDots(Character.OldDaka, Mod.Multiplexer, "1");
     }
 
     @Test
-    public void testTwoDots() {
+    void testTwoDots() {
         assertNumberOfDots(Character.RoyalGuard, Mod.Receiver, "2");
     }
 
     @Test
-    public void testThreeDots() {
+    void testThreeDots() {
         assertNumberOfDots(Character.HothRebelScout, Mod.HoloArray, "3");
     }
 
     @Test
-    public void testFourDots() {
+    void testFourDots() {
         assertNumberOfDots(Character.StormtrooperHan, Mod.DataBus, "4");
     }
 
     @Test
-    public void testFiveDots() {
+    void testFiveDots() {
         assertNumberOfDots(Character.StormtrooperHan, Mod.Multiplexer, "5");
     }
 
